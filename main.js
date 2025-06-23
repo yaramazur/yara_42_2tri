9bbfd79136926c26eff40ab3b4d7ecfb8e0a3cea
yaramazur <yara.marcondes@escola.pr.gov.br>
2025-06-23 19:12:07 -0300

    y

--- main.js
+++ main.js
@@ -21,15 +21,35 @@ numeroSenha.textContent = tamanhoSenha;
 geraSenha();
 }
 const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+for (i=0; i < checkbox.length;i++){
+checkbox[i].onclick = geraSenha;
+}
 const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolos = '!@%#*?';
 geraSenha();
 function geraSenha() {
-let senha = '';
+let alfabeto = '';
+if(checkbox[0].checked){
+alfabeto = alfabeto + letrasMaiusculas;
+}
+if (checkbox[1].checked){
+alfabeto = alfabeto + letrasMinusculas;
+}
+if (checkbox[2].checked){
+alfabeto = alfabeto + numeros;
+}
+if (checkbox[3].checked){
+alfabeto = alfabeto + simbolos;
+}
 
+let senha = '';
 for (let i = 0; i < tamanhoSenha; i++) {
-let numeroAleatorio = Math.random() * letrasMaiusculas.length;
+let numeroAleatorio = Math.random() * alfabeto.length;
 numeroAleatorio = Math.floor(numeroAleatorio);
-senha = senha + letrasMaiusculas[numeroAleatorio];
+senha = senha + alfabeto[numeroAleatorio];
 }
 campoSenha.value = senha;
 }
