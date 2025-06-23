633e16338b2cd4979e173440253674c5fd19dc4d
yaramazur <yara.marcondes@escola.pr.gov.br>
2025-06-23 15:33:52 -0300

    y

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 5;
 numeroSenha.textContent = tamanhoSenha;
+geraSenha();
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
@@ -10,6 +11,7 @@ if (tamanhoSenha > 1){
 tamanhoSenha--;
 }
 numeroSenha.textContent = tamanhoSenha;
+geraSenha();
 }
 function aumentaTamanho(){
 if (tamanhoSenha < 20){
@@ -41,4 +43,16 @@ function geraSenha() {
         senha = senha + letrasMaiusculas[numeroAleatorio];
     }
     campoSenha.value = senha;
+}
+const campoSenha = document.querySelector('#campo-senha');
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+geraSenha();
+function geraSenha() {
+let senha = '';
+for (let i = 0; i < tamanhoSenha; i++) {
+let numeroAleatorio = Math.random() * letrasMaiusculas.length;
+numeroAleatorio = Math.floor(numeroAleatorio);
+senha = senha + letrasMaiusculas[numeroAleatorio];
+}
+campoSenha.value = senha;
 }
